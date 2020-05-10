0c57bd3dff794fbbb96dd47448238fe6d280b5f3
Krystian Maślak <kmaslak96@gmail.com>
2020-05-10 21:41:54 +0200

    Adding BEM, DOM, to improve sections and articles

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ const firstName = "KRiS";
 const age = 24;
 
 console.log(`Siemano, z tej strony ${firstName} i mam ${age} lata.`);
+
+
+const starting = document.querySelector('.lets-go');
+
+starting.innerHTML = `NO TO ZACZYNAMY! ;)`;
