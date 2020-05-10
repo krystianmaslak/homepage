62babe87198fc5dd06af34f1e30e3e984149cc7a
Krystian Maślak <kmaslak96@gmail.com>
2020-05-10 21:46:59 +0200

    Formatting documents

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const age = 24;
 
 console.log(`Siemano, z tej strony ${firstName} i mam ${age} lata.`);
 
-
-const starting = document.querySelector('.lets-go');
+const starting = document.querySelector(".lets-go");
 
 starting.innerHTML = `NO TO ZACZYNAMY! ;)`;
