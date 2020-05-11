27505e24b53caebcdcbf12450ecd02ad3afe4206
Krystian Maślak <kmaslak96@gmail.com>
2020-05-11 21:01:04 +0200

    Optimizing BEM

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const age = 24;
 
 console.log(`Siemano, z tej strony ${firstName} i mam ${age} lata.`);
 
-const starting = document.querySelector(".lets-go");
+const heading = document.querySelector(".main__heading--js");
 
-starting.innerHTML = `NO TO ZACZYNAMY! ;)`;
+heading.innerHTML = `Witaj Milordzie na mojej stronie ;)`;
+
+
+const emptyHeading = document.querySelector(".done-section__heading--js");
+
+emptyHeading.innerHTML = `NO TO ZACZYNAMY! ;)`;
